974b8850e612fa3cc8e30daba02321a2620febb9
johancberg <johancberg@outlook.com>
2020-10-22 11:58:51 +0200

    Toogle between login and register

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Firebase imports
 import firebase from 'firebase/app';
@@ -6,7 +6,8 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const SignIn = ({ auth }) => {
-    let registerPage = true;
+    const [ registerPage, setRegisterPage ] = useState(true)
+
     const signInWithGoogle = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
         auth.signInWithPopup(provider);
@@ -19,15 +20,15 @@ const SignIn = ({ auth }) => {
                     <div><label>E-mail </label><input type="name"></input></div>
                     <div><label>Password </label><input type="password"></input></div>
                 </form>
-            <div>
+            <div className="login-buttons">
                 <button className="sign-in" onClick={ signInWithGoogle }>{ registerPage ? 'Register' : 'Login' }</button>
                 <button className="sign-in google" onClick={ signInWithGoogle }><i className="fab fa-google"></i> Sign In</button>
             </div>
-            {registerPage ?
-                <div className="switch-login">Already have an account? <span onClick={registerPage=false}>Login here!</span></div>
-            :
-                <div className="switch-login">Don't have an account? <span onClick={registerPage=true}>Register here!</span></div>
-            }
+            {registerPage ? (
+                <div className="switch-login">Already have an account? <span onClick={() => setRegisterPage(false)}>Login here!</span></div>
+            ) : (
+                <div className="switch-login">Don't have an account? <span onClick={() => setRegisterPage(true)}>Register here!</span></div>
+            )}
         </div>
     )
 }
