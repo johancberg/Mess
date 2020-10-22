6246d9b3ff440b7056bfce80aa22013b1745160f
johancberg <johancberg@outlook.com>
2020-10-22 10:59:58 +0200

    Beggining of e-mail auth

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -6,6 +6,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const SignIn = ({ auth }) => {
+    let registerPage = true;
     const signInWithGoogle = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
         auth.signInWithPopup(provider);
@@ -13,8 +14,20 @@ const SignIn = ({ auth }) => {
 
     return (
         <div className="main-page">
-            <h2>You're not logged in</h2>
-            <button className="sign-in " onClick={ signInWithGoogle }><i className="fab fa-google"></i> Sign In</button>
+                <form onSubmit="registerAccount">
+                    <h2>{ registerPage ? 'Register' : 'Login' }</h2>
+                    <div><label>E-mail </label><input type="name"></input></div>
+                    <div><label>Password </label><input type="password"></input></div>
+                </form>
+            <div>
+                <button className="sign-in" onClick={ signInWithGoogle }>{ registerPage ? 'Register' : 'Login' }</button>
+                <button className="sign-in google" onClick={ signInWithGoogle }><i className="fab fa-google"></i> Sign In</button>
+            </div>
+            {registerPage ?
+                <div className="switch-login">Already have an account? <span onClick={registerPage=false}>Login here!</span></div>
+            :
+                <div className="switch-login">Don't have an account? <span onClick={registerPage=true}>Register here!</span></div>
+            }
         </div>
     )
 }
