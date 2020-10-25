07ff22776a7d960106631231e2f8c706ef6dc56d
johancberg <johancberg@outlook.com>
2020-10-25 15:16:50 +0100

    Rinsed and tweaked some functions for password

--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -6,17 +6,10 @@ const PasswordInput = ({ label, stateType, value }) => {
 
     return (
     <div><label>{label}</label>
-        <input type={ showPassword ? 'text' : 'password' } value={value} onFocus={ () => setShowEye(true) } onBlur={ () => setShowEye(false) } onChange={e => stateType(e.target.value)}></input>
+        <input type={ showPassword ? 'text' : 'password' } value={value} onFocus={ () => setShowEye(true) } onBlur={ () => { setShowEye(false); setShowPassword(false) } } onChange={e => stateType(e.target.value)}></input>
         { showEye ? <i onMouseOver={() => setShowPassword(true)} onMouseLeave={() => setShowPassword(false)} className={ showPassword ? "fas fa-eye" : "fas fa-eye-slash"}></i> : '' }
     </div>
     )
 }
 
 export default PasswordInput;
-
-/*
-    <div><label>Rewrite Password</label>
-        <input type={ showPassword.rewrite ? 'text' : 'password' } onFocus={ () => setShowEye({rewrite: true}) } onBlur={ () => setShowEye({rewrite: false}) } onChange={e => setStateRewritePassword(e.target.value)}></input>
-        { showEye.rewrite ? <i onMouseOver={() => setShowPassword({rewrite: true})} onMouseLeave={() => setShowPassword({rewrite: false})} className={ showPassword.rewrite ? "fas fa-eye" : "fas fa-eye-slash"}></i> : '' }
-    </div>
-*/
