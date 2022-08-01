5465c2eb2cfe6b3b5ed4aa05f00eeabd9873c802
johancberg <johancberg@outlook.com>
2022-08-02 00:07:36 +0200

    Cleared the unnecessary useEffect

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Users = ({ chats }) => {
-    const [chatList, setChatList] = useState([])
-
-    useEffect(() => {
-        setChatList(chats)
-    }, [])
+    const [chatList] = useState(chats)
 
     return (
         <div style={{position:'relative',top:'10vh'}}>
         {
             chatList.length && chatList.map(msg =>
-                <Link to={`c/${msg.id}`} key={msg.id} ><p style={{color:'white'}}>Chat with {msg.id}</p></Link>
+                <Link to={`/${msg.id}`} key={msg.id} ><p style={{color:'white'}}>Chat with {msg.id}</p></Link>
             )
         }
         </div>
