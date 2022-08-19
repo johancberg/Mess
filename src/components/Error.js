c5d582af658107e973da620cd9034ba1d32fdfb1
johancberg <johancberg@outlook.com>
2022-08-20 00:15:55 +0200

    Add a different error type

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
-const Error = () => {
+const Error = ({error = '404'}) => {
 
     return (
+        <>{ error === '403' ?
         <div className="error-page">
-            <h1 className="error-text">Error 404</h1>
-            <p className="error-text">You have entered a page that doesn't exist or where you ain't granted access.</p>
+            <h1 className="error-text">403 Forbidden</h1>
+            <p className="error-text">You have entered a page where you ain't granted access.</p>
         </div>
+        :
+        <div className="error-page">
+            <h1 className="error-text">404 Not Found</h1>
+            <p className="error-text">You have entered a page that doesn't exist.</p>
+        </div>
+        }</>
     )
 }
 
