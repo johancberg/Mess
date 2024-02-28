0b6b90fe92b314f98876c2c5ba99cc300d1fd956
johancberg <johancberg@outlook.com>
2024-02-29 00:30:17 +0100

    Clear the component from unneeded content

--- src/components/TempChatRoom.js
+++ src/components/TempChatRoom.js
@@ -1,9 +1,5 @@
 import React, {useRef, useState, useEffect} from 'react';
 
-// Components
-import ChatMessage from './ChatMessage';
-import Error from './Error';
-
 // Firebase imports
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -12,25 +8,29 @@ import 'firebase/auth';
 import { useLocation } from 'react-router-dom';
 
 const TempChatRoom = ({ auth, firestore }) => {
+    const [chatParam, setChatParam] = useState()
     const scroll = useRef();
-    const [messages, setMessages] = useState([])
-    const [warning, setWarning] = useState(false)
 
     const searchParams = useQuery();
-    const chatParam = searchParams.get("id");
-    const chatRef = firestore.collection('chats').doc(chatParam);
-    const messageRef = firestore.collection('messages').orderBy('createdAt').where('cid', '==', chatParam);
+    const userParam = searchParams.get("id");
+    const userRef = firestore.collection('user').doc(userParam);
+    const messageRef = firestore.collection('messages').orderBy('createdAt').where('cid', '==', userParam);
 
     const [formValue, setFormValue] = useState('');
     const [reply, setReply] = useState(false);
     
+    const generateChatParam = () => {
+        let result = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        for (let i = 0; i < 20; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
 
     function useQuery() {
       return new URLSearchParams(useLocation().search);
-    }
-
-    const scrollToBottom = () => {
-        window.scrollTo(0, document.body.scrollHeight);
     }
 
     // TODO: Solve "messageRef.set is not a function"
@@ -60,57 +60,21 @@ const TempChatRoom = ({ auth, firestore }) => {
                 photoURL
             });
         }
-        await chatRef.update({ recentPost: firebase.firestore.FieldValue.serverTimestamp() })
+        await userRef.update({ recentPost: firebase.firestore.FieldValue.serverTimestamp() })
         setFormValue('');
         scroll.current.scrollIntoView({ behavior: 'smooth' });
     }
 
-    // Checks if the user should be able to read this chat
+    // Generate chatparameter
     useEffect(() => {
-        firestore.collection('chats').doc(chatParam).get()
-        .then(snapshot => {
-            const cidQuery = snapshot.data()
-            const { uid } = auth.currentUser
-            if (!cidQuery.users.some(user => user === uid)) {
-                setWarning(true)
-            }
-        })
-    }, [firestore, auth.currentUser, chatParam])
-
-    // Scrolls the chat to the bottom
-    useEffect(() => {
-        window.onload = scrollToBottom()
-    }, [messages])
-
-    // Gets all the chat-id-messages
-    useEffect(() => {
-        messageRef.get()
-        .then(querySnapshot => {
-            const msg = []
-            querySnapshot.forEach(doc => {
-                msg.push(doc.data())
-            });
-            setMessages(msg)
-        }).catch(e => console.log(e))
-    }, [chatParam])
+        setChatParam(generateChatParam())
+    }, [])
 
     return (
-        warning
-        ?
-        <Error error="403" />
-        :
         <>
             <main className="message-box">
-                { messages.map((msg, index) => <ChatMessage auth={auth} key={index} message={msg} setReply={setReply} />)}
-                <div ref={scroll}></div>
             </main>
 
-            { reply.message &&
-                <div className="reply-field">
-                    <h3>Reply to: "{ reply.message.text }"</h3>
-                    <div onClick={() => setReply(false)} className="reply-exit"><i className="fas fa-times"></i></div>
-                </div>
-            }
             <form className="input-message" onSubmit={ sendMessage }>
                 <input value={formValue} onChange={(e) => setFormValue(e.target.value) } placeholder={"Type something"}/>
                 <button type="submit" disabled={!formValue} ><i className="fas fa-paper-plane"></i></button>
